e9cc89e0eae565757030ef1bb3e9687015528d2c
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2021-12-08 22:53:41 +0300

    Fix pagination, add pagination for small screens (check it! looks nice), fix template tag

--- sel_film/sel_film/settings.py
+++ sel_film/sel_film/settings.py
@@ -66,23 +66,23 @@ WSGI_APPLICATION = 'sel_film.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'films_1',
-        'USER': 'kirillandreev',
-        'PASSWORD': 'qweasdzxc',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'films_1',
+#         'USER': 'kirillandreev',
+#         'PASSWORD': 'qweasdzxc',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- sel_film/sel_film/urls.py
+++ sel_film/sel_film/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('films.urls')),
     path('serials/', include('serials.urls')),
 ]
