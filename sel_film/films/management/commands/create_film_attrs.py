4cbec6bbf4a26080aa39da37088221d0ca49410a
Kirill Andreev <hizenberg228@mail.ru>
2021-12-10 15:46:22 +0300

    Upgraded func create_film_attrs

--- sel_film/films/management/commands/create_film_attrs.py
+++ sel_film/films/management/commands/create_film_attrs.py
@@ -8,10 +8,11 @@ class Command(BaseCommand):
     help = 'Create Country, Genre, Actor, Director instance for films in DB'
 
     def handle(self, *args, **options):
-        self.write_countries_in_db()
-        self.write_genres_in_db()
-        self.write_actors_in_db()
-        self.write_directors_in_db()
+        all_films_json = self.get_all_films_from_json()
+        self.write_countries_in_db(all_films_json)
+        self.write_genres_in_db(all_films_json)
+        self.write_actors_in_db(all_films_json)
+        self.write_directors_in_db(all_films_json)
         print('Все модели успешно записаны в базу данных')
 
     def get_all_films_from_json(self):
@@ -19,26 +20,25 @@ class Command(BaseCommand):
             objects = json.loads(file.read())
         return objects
 
-    def create_obj_set(self, obj):
+    def create_obj_set(self, obj, films):
         obj_list = []
-        films = self.get_all_films_from_json()
         for film in films:
             for object in film[f'{obj}']:
                 obj_list.append(object)
         return set(obj_list)
 
-    def write_countries_in_db(self):
-        for country in self.create_obj_set('countries'):
+    def write_countries_in_db(self, all_films):
+        for country in self.create_obj_set(obj='countries', films=all_films):
             Country.objects.create(title=country)
         print('Объекты стран успешно созданы')
 
-    def write_genres_in_db(self):
-        for genre in self.create_obj_set('genres'):
+    def write_genres_in_db(self, all_films):
+        for genre in self.create_obj_set(obj='genres', films=all_films):
             Genre.objects.create(title=genre)
         print('Объекты жанров успешно созданы')
 
-    def write_actors_in_db(self):
-        for actor in self.create_obj_set('actors'):
+    def write_actors_in_db(self, all_films):
+        for actor in self.create_obj_set(obj='actors', films=all_films):
             actor_splited = actor.split()
             if len(actor_splited) == 1:
                 Actor.objects.create(first_name=actor_splited[0], last_name='')
@@ -46,8 +46,8 @@ class Command(BaseCommand):
                 Actor.objects.create(first_name=actor_splited[0], last_name=' '.join(actor_splited[1:]))
         print('Объекты актеров успешно созданы')
 
-    def write_directors_in_db(self):
-        for director in self.create_obj_set('directors'):
+    def write_directors_in_db(self, all_films):
+        for director in self.create_obj_set(obj='directors', films=all_films):
             director_splited = director.split()
             if len(director_splited) == 1:
                 Director.objects.create(first_name=director_splited[0], last_name='')
