4b5fbf61dff22ec5da7afc813c8eed5de832ec47
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2022-01-13 19:45:48 +0300

    Add serial list page

--- sel_film/serials/urls.py
+++ sel_film/serials/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 
-from .views import SerialListView, search_serials, SerialDetailView
+from .views import SerialListView, search_serials, SerialDetailView, CatalogSerialListView
 
 urlpatterns = [
-    path('', SerialListView.as_view(), name='serial-list'),
+    path('', CatalogSerialListView.as_view(), name='serial-list'),
     path('search_serials/', search_serials, name='search-serials'),
     path('<int:pk>/', SerialDetailView.as_view(), name='serial-detail'),
 ]
