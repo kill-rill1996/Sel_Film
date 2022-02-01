7a5d77262e6aef3929fa0bee0729baa14a9f4846
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2022-02-01 16:47:50 +0300

    Fix some bugs

--- sel_film/sel_film/middleware.py
+++ sel_film/sel_film/middleware.py
@@ -14,7 +14,6 @@ class ErrorLogMiddleware:
     # TO DO: page for exceptions
     def process_exception(self, request, exception):
         logger.error(f'Error middleware: {exception}')
-        print(request.META.get('HTTP_REFERER'))
         return render(request, 'includes/404.html')
 
 
