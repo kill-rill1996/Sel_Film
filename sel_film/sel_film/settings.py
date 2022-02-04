5af7f0336ac9bb6c8c2412e17547589ac99e1249
Kirill Andreev <hizenberg228@mail.ru>
2022-02-04 18:44:21 +0300

    Changed EMAIL USE SSL

--- sel_film/sel_film/settings.py
+++ sel_film/sel_film/settings.py
@@ -191,7 +191,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = bool(int(os.environ.get('EMAIL_USE_TLS')))
+# EMAIL_USE_TLS = bool(int(os.environ.get('EMAIL_USE_TLS')))
 EMAIL_USE_SSL = bool(int(os.environ.get('EMAIL_USE_SSL')))
 
 # Sentry monitoring
